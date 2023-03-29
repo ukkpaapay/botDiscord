7b07f40e91e0778f1c4b5164ac13da6e23fd7156
ukkpaapay <ukristkrit@hotmail.com>
2023-03-29 23:33:22 +0700

    bot v1

--- himari.js
+++ himari.js
@@ -1,20 +1,56 @@
-const {Client,GatewayIntentBits} = require('discord.js');
-require('dotenv').config()
-const client = new Client({ intents :[
-  GatewayIntentBits.GuildMembers,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.MessageContent
-]})
-
-client.on('ready', () => {
-  console.log('Himari ready!');
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+const client = new Client({
+  intents: [
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+  ],
 });
-client.on('messageCreate', async(message) => {
-    if (message.author.bot) return
-     console.log(message.author.discriminator);
-  // if (message.content === 'Hi hi~~~~~~~') {
-  //   message.channel.send('Hi hi~~~~~~~');
+
+client.on("ready", () => {
+  console.log("Himari ready!");
+});
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  // console.log(message.attachments.size);
+  // if (
+  //   message.content === "Himari" ||
+  //   message.content === "himari" ||
+  //   message.content === "ฮิมาริ"
+  // ) {
+  //   message.channel.send("ว่าไงคะคุณครู");
   // }
+
+  // if (message.author.discriminator === "1425") {
+  //   message.channel.send(`ชื่อ${message.author.username}หรอคะ ทำไมชื่อแปลกจัง`);
+  // }
+
+  if (message.author.discriminator === "1425") {
+    console.log('มีคนโดนแล้ว!!!');
+    if (message.content !== "") {
+      message.channel.send(
+        `ชื่อ${message.author.username}หรอคะ ทำไมชื่อแปลกจัง`
+      );
+    }
+    if (message.attachments.size > 0) {
+      switch (getRandomInt(2)) {
+        case 0:
+          message.channel.send(`เจ๋งดีนี่คะ`);
+          break;
+        case 1:
+          message.channel.send(`โอ้โห สุดยอดไปเลย`);
+          break;
+        default:
+          null;
+          break;
+      }
+    }
+  }
 });
 client.login(process.env.ID_KEY);
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
